ae8a43fe220cd2345038e41580022080a5c58ecb
Phil Brockwell <nineteen40eight@gmail.com>
2015-06-30 23:37:09 +0100

    null session forgery

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,6 +1,5 @@
 class Api::ItemsController < ApplicationController
-
-  protect_from_forgery with: :null_session
+  
   skip_before_filter :verify_authenticity_token
   
   def index
--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,6 +1,5 @@
 class Api::ReviewsController < ApplicationController
-
-  protect_from_forgery with: :null_session
+  
   skip_before_filter :verify_authenticity_token
 
   def index
